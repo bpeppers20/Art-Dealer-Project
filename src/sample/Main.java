1d7f2dc7ea48ea4b689bc973780576ef0957cec1
STLnick <nickrray@gmail.com>
2020-04-30 22:44:20 -0500

    Created event handler for Close Application button - implemented random deal of 4 cards and added to 'selected cards' hbox

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,8 +1,6 @@
 package sample;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 import java.awt.event.InputEvent;
 
 import javafx.application.Application;
@@ -12,7 +10,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
@@ -20,24 +20,26 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
+    private Stage mainStage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        this.mainStage = primaryStage;
+        primaryStage.setOnCloseRequest(confirmCloseEventHandler);
         //Parent rootP = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
+        // Fill list with string values used to fetch images
+        // in setupCardImageViews()
         ArrayList<String> cards = new ArrayList<>();
-
         for (int i = 0; i < 52; i++) {
             cards.add(String.valueOf(i + 1));
         }
-
-        // Shuffle cards; Not sure if we want or need this
-        //java.util.Collections.shuffle(cards);
-
-
 
         /* * * GRID PANE SETUP * * */
         //Creating a Grid Pane
@@ -56,13 +58,16 @@ public class Main extends Application {
         //Setting the Grid alignment
         gridPane.setAlignment(Pos.CENTER);
 
+        gridPane.setStyle("-fx-background-color: GREEN;");
+        /* * * * * * * * * * * * * */
+
         // Current Card section
         HBox hbox = new HBox();
         hbox.setMinHeight(150);
         hbox.setPadding(new Insets(15, 12, 15, 12));
         hbox.setSpacing(10);
         hbox.setStyle("-fx-background-color: #336699;");
-
+        // Add hbox to gridPane
         gridPane.add(hbox, 0, 7, 8,2);
 
         // Data structure to hold images of all cards
@@ -70,45 +75,34 @@ public class Main extends Application {
         // Fill data structure
         setupCardImageViews(cards, imageViews, hbox);
 
-        // Add all cards in gridPane
-        int j = 0;
-        int i = 0;
-        for(int x = 0; x < 52; x++) {
-            String key = "view" + x;
-            gridPane.add(imageViews.get(key), i, j);
-            // Adding cards to grid with 9 in each Row
-            if((i % 8) == 0 && i != 0) {
-                j++;
-                i = 0;
-            } else {
-                i++;
-            }
-        }
+        // Add cards to GridPane
+        addCards(gridPane, imageViews);
 
+
+        /* ToDo: Need to consider how to implement difficulty selection
+             - have different display at start of game?
+             - allow changing in middle of game?*/
         //Label for difficulty
         Text difficultyLabel = new Text("Difficulty");
-
         //Choice box for difficulty
         ChoiceBox difficultychoiceBox = new ChoiceBox();
         difficultychoiceBox.getItems().addAll
                 ("Easy (K-2)", "Medium (3-5)", "Hard (6-8)");
 
-        // Add to gridPane
+        // Add to difficulty choiceBox gridPane
         gridPane.add(difficultyLabel, 8, 6);
         gridPane.add(difficultychoiceBox, 9, 6);
 
-        // Buttons
-        Button quitBtn = new Button("Quit Game");
-        quitBtn.setOnAction(e -> {
-            // ToDo: Need to add real action
-                // 1. Display alert to ensure they meant to quit
-                    // a. Yes -> end game
-                    // b. No -> remove alert and proceed as normal
 
-            // Test output
-            System.out.println("Quit was clicked!");
-        });
+        /* * * Buttons * * */
 
+        // -- Close Application --
+        Button closeButton = new Button("Close Application");
+        closeButton.setOnAction(event ->
+                primaryStage.fireEvent(new WindowEvent(primaryStage, WindowEvent.WINDOW_CLOSE_REQUEST))
+        );
+
+        // -- Make a Guess --
         Button guessBtn = new Button("Make a Guess");
         guessBtn.setOnAction(e -> {
             // ToDo: Need to add real action
@@ -126,21 +120,39 @@ public class Main extends Application {
             System.out.println("Guess was clicked!");
         });
 
+        // -- Random Deal --
         Button randomDealBtn = new Button("Random Deal");
         randomDealBtn.setOnAction(e -> {
-            // ToDo: Need to add real action
-                // 1. Select 4 cards randomly
-                // 2. Show 4 cards in 'deal spot'
+            // Implement the Card Class once that is complete
 
-            // Test output
-            System.out.println("Random Deal was clicked!");
+
+            // ArrayList holding 0-51 to access imageViews indices
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            for (int k = 0; k < 52; k++) {
+                list.add(new Integer(k));
+            }
+            Collections.shuffle(list);  // Shuffle list of 0-51 to 'randomize' it
+
+            // Now grab 4 'random numbers' from the shuffled list to grab cards
+            ImageView card1 = imageViews.get("view" + list.get(0));
+            ImageView card2 = imageViews.get("view" + list.get(1));
+            ImageView card3 = imageViews.get("view" + list.get(2));
+            ImageView card4 = imageViews.get("view" + list.get(3));
+
+            // Add 4 random cards to selected cards box
+            hbox.getChildren().addAll(card1, card2, card3, card4);
+
+            // Disable Random Deal button
+            randomDealBtn.setDisable(true);
+
+            // ToDo: Still need to re-enable the button once the next turn starts
+
         });
 
+        // Add buttons to GridPane
         gridPane.add(randomDealBtn, 9, 0);
         gridPane.add(guessBtn, 9, 1);
-        gridPane.add(quitBtn, 9, 2);
-
-        gridPane.setStyle("-fx-background-color: GREEN;");
+        gridPane.add(closeButton, 9, 2);
 
         Scene scene = new Scene(gridPane);
 
@@ -161,6 +173,42 @@ public class Main extends Application {
             });
         }
     }
+
+    public void addCards(GridPane gridPane, Map<String, ImageView> imageViews) {
+        // Add all cards in gridPane
+        int j = 0;
+        int i = 0;
+        for(int x = 0; x < 52; x++) {
+            String key = "view" + x;
+            gridPane.add(imageViews.get(key), i, j);
+            // Adding cards to grid with 9 in each Row
+            if((i % 8) == 0 && i != 0) {
+                j++;
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private EventHandler<WindowEvent> confirmCloseEventHandler = event -> {
+        Alert closeConfirmation = new Alert(
+                Alert.AlertType.CONFIRMATION,
+                "Are you sure you want to exit?"
+        );
+        Button exitButton = (Button) closeConfirmation.getDialogPane().lookupButton(
+                ButtonType.OK
+        );
+        exitButton.setText("Exit");
+        closeConfirmation.setHeaderText("Confirm Exit");
+        closeConfirmation.initModality(Modality.APPLICATION_MODAL);
+        closeConfirmation.initOwner(mainStage);
+
+        Optional<ButtonType> closeResponse = closeConfirmation.showAndWait();
+        if (!ButtonType.OK.equals(closeResponse.get())) {
+            event.consume();
+        }
+    };
 
     public static void main(String[] args) {
         launch(args);
